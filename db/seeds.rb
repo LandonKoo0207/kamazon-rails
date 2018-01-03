d4b07202430db38b1e8cead6fa5246cf68a92e64
Landon Koo <landonkoo0207@gmail.com>
2018-01-03 16:21:00 +1100

    added the products table

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+
+niceCat = Product.create(title: "Quality Kitty",
+  subtitle: "Suri the Pretty Cat", image_name: "suri_stare.JPG",
+  price: "4.99", sku: "kittyOne", download_url: "https://s3-ap-southeast-2.amazonaws.com/rails-kamazon-bucket/suri_stare.jpg" )
